454537cb77f4722e492b8720491dc6ece6a19e15
Yashraj Monani <yashrajm16802@gmail.com>
2023-07-22 09:59:07 +0530

    first commit

---
+++ day 1.py
@@ -0,0 +1,23 @@
+#python
+
+#brackets
+#identifiers
+#operators
+#data and types
+#built in function for types 
+#control statements
+#conditional
+#looping
+#comprehensions
+#user defined function
+#function parameterization
+#lambda expression
+
+#user confoguration
+#git config --global user.name "yashraj monani"
+#git config --global user.email "yashrajm16802@gmail.com"
+#daily process
+#save and commit all changes
+#git add . -> stage
+#git commit -m "first time commit"
+#to use global -for one github accounts and not use global multiple acc
