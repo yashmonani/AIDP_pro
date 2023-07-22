7659b77af1fe30e291ec0a2aa1576a0c1c6e4eeb
Yashraj Monani <yashrajm16802@gmail.com>
2023-07-22 10:05:14 +0530

    first commit 1

--- day 1.py
+++ day 1.py
@@ -16,8 +16,13 @@
 #user confoguration
 #git config --global user.name "yashraj monani"
 #git config --global user.email "yashrajm16802@gmail.com"
+#Remote repository connect
+#git remote add origin "https://github.com/yashmonani/AIDP_pro.git"
+
+
 #daily process
 #save and commit all changes
 #git add . -> stage
 #git commit -m "first time commit"
 #to use global -for one github accounts and not use global multiple acc
+#commit-my work has been done
